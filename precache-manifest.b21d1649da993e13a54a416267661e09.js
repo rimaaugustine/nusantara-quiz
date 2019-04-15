1984ac5413b8d94e2cd63073ef4d17bf7d9ca4be
Rima Faiqoh Augustine <augustine@campus.tu-berlin.de>
2019-04-15 15:17:32 +0200

    Updates

---
+++ precache-manifest.b21d1649da993e13a54a416267661e09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d98074b77f13650183af",
+    "url": "/nusantara-quiz/static/js/runtime~main.fdd8e242.js"
+  },
+  {
+    "revision": "67c1a63b6e0ded3c61a4",
+    "url": "/nusantara-quiz/static/js/main.7d60c02d.chunk.js"
+  },
+  {
+    "revision": "f1962f442c989d4144d4",
+    "url": "/nusantara-quiz/static/js/2.06d53527.chunk.js"
+  },
+  {
+    "revision": "67c1a63b6e0ded3c61a4",
+    "url": "/nusantara-quiz/static/css/main.a22ce7f5.chunk.css"
+  },
+  {
+    "revision": "77d19be895a57189d61f73166c93938b",
+    "url": "/nusantara-quiz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nusantara-quiz/precache-manifest.ccb783707e9363878676b4c194dbc2b9.js"
+  "/nusantara-quiz/precache-manifest.b21d1649da993e13a54a416267661e09.js"
 );
 
 workbox.clientsClaim();
