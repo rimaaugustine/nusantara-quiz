e18fc8d997e1b7d521b83dcd2dcebe9cb17883cd
Rima Faiqoh Augustine <augustine@campus.tu-berlin.de>
2019-04-30 17:01:52 +0200

    preheroku deploy2

---
+++ build/precache-manifest.da29f5bb399fec85a0c5823b1b58caa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df056399da956080d555",
+    "url": "/static/js/main.01812471.chunk.js"
+  },
+  {
+    "revision": "9b629497bfc637cfad05",
+    "url": "/static/js/2.cf3622d7.chunk.js"
+  },
+  {
+    "revision": "df056399da956080d555",
+    "url": "/static/css/main.a22ce7f5.chunk.css"
+  },
+  {
+    "revision": "2e9de0aeb3aa74bdf6adf7cd43ecf1fb",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cbe395c91668d9285737699eda6f669d.js"
+  "/precache-manifest.da29f5bb399fec85a0c5823b1b58caa8.js"
 );
 
 workbox.clientsClaim();
